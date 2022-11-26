19a1f149c8eab47355ea997a8a4765e91ce099fb
Eray Sahin <eraysahinmoon@gmail.com>
2022-11-25 22:09:29 -0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,17 +1,3 @@
-/**
- * products.js
- *
- * The store's products are defined as an Array of product Objects.
- * Each product Object includes the following properties:
- *
- *  - id: String, a unique product identifier (e.g., "P1", "P2")
- *  - name: String, a short name for the product (e.g., "Gingerbread Cookie")
- *  - description: String, a description of the product
- *  - price: Number, the unit price of the item in whole cents (e.g., 100 = $1.00, 5379 = $53.79)
- *  - discontinued: Boolean, whether or not the product has been discontinued
- *  - categories: Array, the category id or ids to which this product belongs (e.g., ["c1"] or ["c1", "c2"])
- */
-
 window.products = [
   {
     id: "P1",
