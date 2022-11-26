1f9dd07e8ea7b5d32b57d2a59480eeb48a397469
Eray Sahin <eraysahinmoon@gmail.com>
2022-11-25 22:09:16 -0500

    Update categories.js

--- js/categories.js
+++ js/categories.js
@@ -1,12 +1,4 @@
-/**
- * categories.js
- *
- * The store's categories are defined in an Array of category Objects. Each
- * category Object has the following properties:
- *
- *  - id: String, a unique category id (e.g., "c1", "c2", "c3")
- *  - description: String, the human-readable name for the category (e.g., "Perfume")
- */
+
 
 window.categories = [
   { id: "c1", description: "All" },
